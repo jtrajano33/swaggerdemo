64c2295e16ed4514fd1dab5e5b6fc1bf8cd2a827
Justin Trajano <justin@intimate.io>
2019-03-19 09:21:51 +0800

    added currency on thakyou page

--- app/containers/Checkout/reducer.js
+++ app/containers/Checkout/reducer.js
@@ -5,10 +5,15 @@
  */
 
 import { fromJS } from 'immutable';
-import { DEFAULT_ACTION, TRANSACTION_CURRENCY } from './constants';
+import {
+  DEFAULT_ACTION,
+  TRANSACTION_CURRENCY,
+  MODE_OF_PAYMENT,
+} from './constants';
 
 export const initialState = fromJS({
   transactionPayload: null,
+  modeOfPayment: null,
 });
 
 function checkoutReducer(state = initialState, action) {
@@ -18,6 +23,9 @@ function checkoutReducer(state = initialState, action) {
     case TRANSACTION_CURRENCY:
       // console.log('REDUCER',action.payload);
       return state.set('transactionPayload', action.payload);
+    case MODE_OF_PAYMENT:
+      return state.set('modeOfPayment', action.payload);
+
     default:
       return state;
   }
